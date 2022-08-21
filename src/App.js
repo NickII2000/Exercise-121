1ea68b82286ee7808f7debc9833d395f49df458d
Nick <Nick_II2000@mail.ru>
2022-08-21 21:57:17 +0600

    28-th commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p><b>"Моя твоя понимает!"</b></p>
-        <p><b>"Моя твоя понимает!"</b></p>
+        <p><b>" Моя твоя понимает! "</b></p>
         <p><b>"Моя твоя понимает!"</b></p>
         <a
           className="App-link"
