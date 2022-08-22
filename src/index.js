1cec86c8c92c65dc41d868ff94923cfe1abda08d
Nick <Nick_II2000@mail.ru>
2022-08-22 21:20:35 +0600

    48-th commit

--- src/index.js
+++ src/index.js
@@ -11,9 +11,12 @@ import App from './App';
 
 // const elem = <h2>Hello World!</h2>;
 // const elem = React.createElement('h2', { className: 'greetings' }, 'Hello World!');
+
+const text = 'Hello World!';
+
 const elem = (
   <div>
-    <h2>Hello World</h2>
+    <h2>{text}</h2>
     <input type='text' />
     <button>Кнопочка</button>
     <button />
