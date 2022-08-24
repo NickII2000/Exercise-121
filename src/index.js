ead259295decd8360c340da16790ee9d33f909e3
Nick <Nick_II2000@mail.ru>
2022-08-24 10:30:11 +0600

    82-th commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -7,6 +7,8 @@ import { Header } from './App';
 const text = 'Hello World!';
 
 ReactDOM.render(
-  <App />,
+  <StrictMode>
+    <App />
+  </StrictMode>,
   document.getElementById('root')
 );
