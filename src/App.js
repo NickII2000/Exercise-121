4044b43c773329f82b4725fa9757d58ee9544397
Nick <Nick_II2000@mail.ru>
2022-08-24 09:20:34 +0600

    66-th commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ const Header = () => {
   return <h2>Hello World!</h2>
 }
 
+const Field = () => {
+  return <input placeholder='Type here' type='text' />
+}
+
+function Btn() {
+  return <button />
+}
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Field />
+      <Btn />
     </div>
   );
 }
