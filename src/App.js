4a467f7c78f606d42e158b76ba082e34faa3a369
Nick <Nick_II2000@mail.ru>
2022-08-25 09:48:10 +0600

    86-th commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { Component, StrictMode } from 'react';
 import './App.css';
 
-const Header = () => {
-  return <h2>Hello World!</h2>
-}
+const Header = () => <h2>Hello World!</h2>;
 
 // const Field = () => {
 //   const holder = 'Enter here';
