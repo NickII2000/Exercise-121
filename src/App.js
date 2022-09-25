cd417028a0218ca6bc51d05b641f05d666273ea1
Nick <Nick_II2000@mail.ru>
2022-09-25 14:49:32 +0600

    89-th commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,26 @@
-import { Component, StrictMode } from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-// const Header = () => {
-//   return <h2>Hello World!</h2>
-// }
-
-const Header = () => <h2>Hello World!!!</h2>;
-
-// const Field = () => {
-//   const holder = 'Enter here';
-//   const styledField = {
-//     width: '300px',
-//     height: '50px'
-//   };
-//   return <input
-//     placeholder={holder}
-//     type='text'
-//     style={styledField} />;
-// }
-
-class Field extends Component {
-  render() {
-    const holder = 'Enter here';
-    const styledField = {
-      width: '300px',
-      height: '50px'
-    };
-    return <input
-      placeholder={holder}
-      type='text'
-      style={styledField} />;
-  }
-}
-
-function Btn() {
-  const text = 'Log in';
-  const logged = false;
-
-
-  const p = <p>Log in</p>;
-  return <button>{logged ? 'Enter' : text}</button>;
-}
 
 function App() {
   return (
     <div className="App">
-      <StrictMode>
-        <Header />
-      </StrictMode>
-      <Field />
-      <Btn />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p><b>"Моя твоя понимает!"</b></p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
-export { Header };
 export default App;
